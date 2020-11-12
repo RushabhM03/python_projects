97221d827ba74db0a869f51768903c1b6933d9cf
Rushabh03 <rushabh.maru123@gmail.com>
2020-11-12 14:02:12 +0530

    python project

--- mad_libs_generator.py
+++ mad_libs_generator.py
@@ -12,7 +12,8 @@ class madlibs:
         print("For a duck may be somebody's", noun2, ",")
         print("Be kind to your", p_noun, "in", place)
         print("Where the weather is always", adj, ".")
-        print()
+        for i in range(5):
+            print()
         print("You may think that is this the", noun3, ",")
         print("Well it is.")
         print("------------------------------------------")
